78eb673d104f8733d2717e7c463324c6cbd0b651
SimonB00 <simone.balducci@gmail.com>
2023-01-31 19:01:21 +0100

    Added applyKernel method. Fixed typo in gaus constructor

--- CLUEstering/include/Kernels.h
+++ CLUEstering/include/Kernels.h
@@ -12,15 +12,28 @@ class kernel{
 		float m_gaus_std;
 		float m_gaus_amplitude;
 		// Parameters of exponential function
-		float m_exp_amplitude;
 		float m_exp_avg;
+		float m_exp_amplitude;
 		// Parameter of flat function
 		float m_flat;
+
+		// Distinguish the type of kernel
+		bool m_isGaus = false;
+		bool m_isExp = false;
+		bool m_isFlat = false;
 	public:
 		kernel(float avg, float std, float amplitude) 
-			: m_gaus_avg(avg), m_gaus_std(std), m_exp_amplitude(amplitude) {}
-		kernel(float avg, float amplitude) : m_exp_avg(avg), m_exp_amplitude(amplitude) {}
-		kernel(float flat) : m_flat(flat) {}
+			: m_gaus_avg(avg), m_gaus_std(std), m_gaus_amplitude(amplitude) {
+				m_isGaus = true;
+			}
+		kernel(float avg, float amplitude) 
+			: m_exp_avg(avg), m_exp_amplitude(amplitude) {
+				m_isExp = true;
+		}
+		kernel(float flat) 
+			: m_flat(flat) {
+				m_isFlat = true;
+		}
 
 		float flat(float dist_ij, int point_id, int j) {
 			if(point_id == j) {
@@ -42,6 +55,16 @@ class kernel{
 			} else {
 				return m_exp_amplitude*exp(-m_exp_avg*dist_ij);
 			}
+		}
+
+		float applyKernel(float dist_ij, int point_id, int j) {
+			if(m_isGaus) {
+				return gaussian(dist_ij, point_id, j);
+			} else if(m_isExp) {
+				return exponential(dist_ij, point_id, j);
+			} else  { // assume that at least one is going to be true
+				return flat(dist_ij, point_id, j);
+			} 
 		}
 };
 
