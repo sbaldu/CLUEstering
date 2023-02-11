deb1aaa94504bbc0ca2dac0ba5082d484b560dbe
SimonB00 <simone.balducci@gmail.com>
2023-02-11 17:41:18 +0100

    Added include <pybind11/functional.h> in binding.cc

--- CLUEstering/binding.cc
+++ CLUEstering/binding.cc
@@ -3,206 +3,175 @@
 #include "include/Clustering.h"
 #include "include/Kernels.h"
 
-#include <stdint.h>
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h> 
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <stdint.h>
 
 /////////////////////
 //////  Run.h  //////
 /////////////////////
-std::vector<std::vector<int>> run1(float dc, 
-											  float rhoc, 
-											  float outlier, 
-											  int pPBin, 
-											  kernel ker,
-		                             std::vector<std::vector<float>> const& coordinates, 											 
-											  std::vector<float> const& weight) {
-	ClusteringAlgo<1> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run1(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<1> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> run2(float dc, 
-		 									  float rhoc, 
-											  float outlier, 
-											  int pPBin, 
-											  kernel ker,
-											  std::vector<std::vector<float>> const& coordinates,
-											  std::vector<float> const& weight) {
-	ClusteringAlgo<2> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run2(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<2> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
-}
-			
-std::vector<std::vector<int>> run3(float dc, 
-											  float rhoc, 
-											  float outlier, 
-											  int pPBin, 
-											  kernel ker,
-											  std::vector<std::vector<float>> const& coordinates,
-											  std::vector<float> const& weight) {
-	ClusteringAlgo<3> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
-
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> run4(float dc, 
-											  float rhoc, 
-											  float outlier, 
-											  int pPBin, 
-											  kernel ker,
-											  std::vector<std::vector<float>> const& coordinates,
-											  std::vector<float> const& weight) {
-	ClusteringAlgo<4> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run3(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<3> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> run5(float dc, 
-											  float rhoc, 
-											  float outlier, 
-											  int pPBin, 
-											  kernel ker,
-	  										  std::vector<std::vector<float>> const& coordinates,
-											  std::vector<float> const& weight) {
-	ClusteringAlgo<5> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run4(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<4> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> run6(float dc, 
-											  float rhoc, 
-											  float outlier, 
-											  int pPBin, 
-											  kernel ker,
-											  std::vector<std::vector<float>> const& coordinates,
-											  std::vector<float> const& weight) {
-	ClusteringAlgo<6> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run5(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<5> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> run7(float dc, 
-											  float rhoc, 
-											  float outlier, 
-											  int pPBin, 
-											  kernel ker,
-											  std::vector<std::vector<float>> const& coordinates,
-											  std::vector<float> const& weight) {
-	ClusteringAlgo<7> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run6(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<6> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> run8(float dc, 
-											  float rhoc, 
-											  float outlier, 
-											  int pPBin, 
-											  kernel ker,
-											  std::vector<std::vector<float>> const& coordinates, 											 std::vector<float> const& weight) {
-	ClusteringAlgo<8> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run7(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<7> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> run9(float dc, 
-											  float rhoc, 
-											  float outlier, 
-											  int pPBin,  
-											  kernel ker,
-											  std::vector<std::vector<float>> const& coordinates, 											 std::vector<float> const& weight) {
-	ClusteringAlgo<9> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run8(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<8> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> run10(float dc, 
-											   float rhoc, 
-												float outlier, 
-												int pPBin, 
-												kernel ker,
-												std::vector<std::vector<float>> const& coordinates, 
-												std::vector<float> const& weight) {
-	ClusteringAlgo<10> algo(dc,rhoc,outlier,pPBin);
-	algo.setPoints(coordinates[0].size(), coordinates, weight);
+std::vector<std::vector<int>>
+run9(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+     std::vector<std::vector<float>> const &coordinates,
+     std::vector<float> const &weight) {
+  ClusteringAlgo<9> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
 
-	return algo.makeClusters(ker);
+  return algo.makeClusters(ker);
 }
 
-std::vector<std::vector<int>> mainRun(float dc, 
-												  float rhoc, 
-												  float outlier, 
-												  int pPBin, 
-												  kernel ker,
-            								  std::vector<std::vector<float>> const& coords, 
-												  std::vector<float> const& weight, 
-												  int Ndim) {
-	// Running the clustering algorithm //
-	switch(Ndim) {
-		[[unlikely]] case(1):
-			return run1(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[likely]] case(2):
-			return run2(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[likely]] case(3):
-			return run3(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[unlikely]] case(4):
-			return run4(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[unlikely]] case(5):
-			return run5(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[unlikely]] case(6):
-			return run6(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[unlikely]] case(7):
-			return run7(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[unlikely]] case(8):
-			return run8(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[unlikely]] case(9):
-			return run9(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[unlikely]] case(10):
-			return run10(dc,rhoc,outlier,pPBin,ker,coords,weight);
-			break;
-		[[unlikely]] default:
-			std::cout << "This library only works up to 10 dimensions\n";
-			return {};
-			break;
-	}
+std::vector<std::vector<int>>
+run10(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+      std::vector<std::vector<float>> const &coordinates,
+      std::vector<float> const &weight) {
+  ClusteringAlgo<10> algo(dc, rhoc, outlier, pPBin);
+  algo.setPoints(coordinates[0].size(), coordinates, weight);
+
+  return algo.makeClusters(ker);
+}
+
+std::vector<std::vector<int>>
+mainRun(float dc, float rhoc, float outlier, int pPBin, kernel ker,
+        std::vector<std::vector<float>> const &coords,
+        std::vector<float> const &weight, int Ndim) {
+  // Running the clustering algorithm //
+  switch (Ndim) {
+  [[unlikely]] case (1) :
+    return run1(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[likely]] case (2) :
+    return run2(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[likely]] case (3) :
+    return run3(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[unlikely]] case (4) :
+    return run4(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[unlikely]] case (5) :
+    return run5(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[unlikely]] case (6) :
+    return run6(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[unlikely]] case (7) :
+    return run7(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[unlikely]] case (8) :
+    return run8(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[unlikely]] case (9) :
+    return run9(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[unlikely]] case (10) :
+    return run10(dc, rhoc, outlier, pPBin, ker, coords, weight);
+    break;
+  [[unlikely]] default:
+    std::cout << "This library only works up to 10 dimensions\n";
+    return {};
+    break;
+  }
 }
 
 //////////////////////////////
 //////  Binding module  //////
 //////////////////////////////
 PYBIND11_MODULE(CLUEsteringCPP, m) {
-	m.doc() = "Binding for CLUE";
+  m.doc() = "Binding for CLUE";
 
-	pybind11::class_<kernel>(m, "kernel")
-		.def(pybind11::init<float,int,int>())
-		.def("applyKernel", &kernel::applyKernel, "");
-	pybind11::class_<flatKernel>(m, "flatKernel")
-		.def(pybind11::init<float,int,int,float>())
-		.def("applyKernel", &flatKernel::applyKernel, "");
-	pybind11::class_<gaussianKernel>(m, "gaussianKernel")
-		.def(pybind11::init<float,int,int,float,float,float>())
-		.def("applyKernel", &gaussianKernel::applyKernel, "");
-	pybind11::class_<exponentialKernel>(m, "exponentialKernel")
-		.def(pybind11::init<float,int,int,float,float>())
-		.def("applyKernel", &exponentialKernel::applyKernel, "");
+  pybind11::class_<kernel>(m, "kernel")
+      .def(pybind11::init<KernelType>())
+      .def("applyKernel", &kernel::applyKernel, "");
+  pybind11::class_<flatKernel>(m, "flatKernel")
+      .def(pybind11::init<float>())
+      .def("applyKernel", &flatKernel::applyKernel, "");
+  pybind11::class_<gaussianKernel>(m, "gaussianKernel")
+      .def(pybind11::init<float, float, float>())
+      .def("applyKernel", &gaussianKernel::applyKernel, "");
+  pybind11::class_<exponentialKernel>(m, "exponentialKernel")
+      .def(pybind11::init<float, float>())
+      .def("applyKernel", &exponentialKernel::applyKernel, "");
 
-	m.def("mainRun", &mainRun, "mainRun");
+  m.def("mainRun", &mainRun, "mainRun");
 }
