54a9fb4ba4e7e809a818c4ac547fa9a14626f772
samuelelanzi <samuelelanzi00@icloud.com>
2023-02-15 17:21:34 +0100

    Implementation of kernels without inheritance

--- CLUEstering/include/Kernels.h
+++ CLUEstering/include/Kernels.h
@@ -10,69 +10,40 @@
 using KernelType = std::function<float(float, int, int)>;
 
 // Define the kernels used for the calculation of local density
-// The base class allows to define a generic kernel
 class kernel {
 protected:
   KernelType m_kernel_func;
 
 public:
-  kernel() = default;
-  kernel(KernelType kernel_function) {
-    m_kernel_func = std::move(kernel_function);
-  }
+  kernel(KernelType kernel_function)
+      : m_kernel_func{std::move(kernel_function)} {}
 
-  virtual float applyKernel(float dist_ij, int point_id, int j) {
-    return m_kernel_func(dist_ij, point_id, j);
-  }
-};
-
-// Some derived classes are defined to simplify the use of the most common
-// kernels
-class flatKernel : public kernel {
-private:
-  float m_flat;
-
-public:
-  flatKernel(float flat) : m_flat(flat) {
-    m_kernel_func = [=, this](float distij_, int id_, int j_) {
-      if (id_ == j_) {
+  kernel(float flat) {
+    m_kernel_func = [=](float dist_ij, int point_id, int j) {
+      if (point_id == j) {
         return 1.f;
       } else {
-        return m_flat;
+        return flat;
       }
     };
   }
-};
 
-class gaussianKernel : public kernel {
-private:
-  float m_gaus_avg;
-  float m_gaus_std;
-  float m_gaus_amplitude;
-
-public:
-  gaussianKernel(float gaus_avg, float gaus_std, float gaus_amplitude)
-      : m_gaus_avg(gaus_avg), m_gaus_std(gaus_std),
-        m_gaus_amplitude(gaus_amplitude) {
-    m_kernel_func = [=, this](float distij_, int id_, int j_) {
-	  return static_cast<float>(
-		  m_gaus_amplitude *
-		  exp(-pow(distij_ - m_gaus_avg, 2) / (2 * pow(m_gaus_std, 2))));
+  kernel(float gaus_avg, float gaus_std, float gaus_amplitude) {
+    m_kernel_func = [=](float dist_ij, int point_id, int j) {
+      return static_cast<float>(gaus_amplitude *
+                                std::exp(-std::pow(dist_ij- gaus_avg, 2) /
+                                         (2 * std::pow(gaus_std, 2))));
     };
   }
-};
 
-class exponentialKernel : public kernel {
-private:
-  float m_exp_avg;
-  float m_exp_amplitude;
-
-public:
-  exponentialKernel(float exp_avg, float exp_amplitude)
-      : m_exp_avg(exp_avg), m_exp_amplitude(exp_amplitude) {
-    m_kernel_func = [=, this](float distij_, int id_, int j_) {
-	  return static_cast<float>(m_exp_amplitude * exp(-m_exp_avg * distij_));
+  kernel(float exp_avg, float exp_amplitude) {
+    m_kernel_func = [=](float dist_ij, int point_id, int j) {
+      return static_cast<float>(exp_amplitude * exp(-exp_avg * dist_ij));
     };
+  }
+
+  auto const operator()(float dist_ij, int point_id, int j) {
+    return m_kernel_func(dist_ij, point_id, j);
   }
 };
 
