f85966d285e4a51318ab3c8e07867c42396fae6d
SimonB00 <simone.balducci00@gmail.com>
2023-02-25 11:12:42 +0100

    Rewritten kernels as 'empty' base class and four derived classes

--- CLUEstering/include/Kernels.h
+++ CLUEstering/include/Kernels.h
@@ -14,7 +14,7 @@ class kernel {
 public:
   kernel() = default; 
 
-  virtual float operator()(float dist_ij, int point_id, int j) = 0;
+  virtual float operator()(float dist_ij, int point_id, int j) const { return 0.f; };
 };
 
 class flatKernel : public kernel {
@@ -24,7 +24,7 @@ private:
 public:
   flatKernel(float flat) : m_flat(flat) {}
 
-  float operator()(float dist_ij, int point_id, int j) override {
+  float operator()(float dist_ij, int point_id, int j) const override {
     if (point_id == j) {
       return 1.f;
     } else {
@@ -43,7 +43,7 @@ public:
   gaussianKernel(float gaus_avg, float gaus_std, float gaus_amplitude) 
   : m_gaus_avg(gaus_avg), m_gaus_std(gaus_std), m_gaus_amplitude(gaus_amplitude) {}
 
-  float operator()(float dist_ij, int point_id, int j) override {
+  float operator()(float dist_ij, int point_id, int j) const override {
     return static_cast<float>(m_gaus_amplitude *
                               std::exp(-std::pow(dist_ij- m_gaus_avg, 2) /
                                         (2 * std::pow(m_gaus_std, 2))));
@@ -58,7 +58,7 @@ public:
   exponentialKernel(float exp_avg, float exp_amplitude) 
   : m_exp_avg(exp_avg), m_exp_amplitude(exp_amplitude) {}
 
-  float operator()(float dist_ij, int point_id, int j) override {
+  float operator()(float dist_ij, int point_id, int j) const override {
     return static_cast<float>(m_exp_amplitude * exp(-m_exp_avg * dist_ij));
   }
 };
@@ -69,7 +69,7 @@ private:
 public:
   customKernel(kernel_t function) : m_function(std::move(function)) {}
 
-  float operator()(float dist_ij, int point_id, int j) override {
+  float operator()(float dist_ij, int point_id, int j) const override {
     return m_function(dist_ij, point_id, j);
   }
 };
