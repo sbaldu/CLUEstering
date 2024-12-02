f46f2b6b1e05246b2464376bd63fd4239861d11e
Simone Balducci <simone.balducci00@gmail.com>
2024-12-02 14:58:12 +0100

    Rework points as SoA

--- CLUEstering/alpaka/DataFormats/alpaka/PointsAlpaka.h
+++ CLUEstering/alpaka/DataFormats/alpaka/PointsAlpaka.h
@@ -6,65 +6,52 @@
 
 #include "../../AlpakaCore/alpakaConfig.h"
 #include "../../AlpakaCore/alpakaMemory.h"
-#include "AlpakaVecArray.h"
 #include "../Points.h"
-
-using cms::alpakatools::VecArray;
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
   template <uint8_t Ndim>
   class PointsAlpaka {
   public:
+
     PointsAlpaka() = delete;
     explicit PointsAlpaka(Queue stream, int n_points)
-        : coords{cms::alpakatools::make_device_buffer<VecArray<float, Ndim>[]>(stream,
-                                                                               n_points)},
-          weight{cms::alpakatools::make_device_buffer<float[]>(stream, n_points)},
-          rho{cms::alpakatools::make_device_buffer<float[]>(stream, n_points)},
-          delta{cms::alpakatools::make_device_buffer<float[]>(stream, n_points)},
-          nearest_higher{cms::alpakatools::make_device_buffer<int[]>(stream, n_points)},
-          cluster_index{cms::alpakatools::make_device_buffer<int[]>(stream, n_points)},
-          is_seed{cms::alpakatools::make_device_buffer<int[]>(stream, n_points)},
+        : buffer{cms::alpakatools::make_device_buffer<float[]>(stream,
+                                                               (Ndim + 6) * n_points)},
           view_dev{cms::alpakatools::make_device_buffer<PointsAlpakaView>(stream)} {
       auto view_host = cms::alpakatools::make_host_buffer<PointsAlpakaView>(stream);
-      view_host->coords = coords.data();
-      view_host->weight = weight.data();
-      view_host->rho = rho.data();
-      view_host->delta = delta.data();
-      view_host->nearest_higher = nearest_higher.data();
-      view_host->cluster_index = cluster_index.data();
-      view_host->is_seed = is_seed.data();
+      view_host->coords = buffer.data();
+      view_host->weight = buffer.data() + Ndim * n_points;
+      view_host->rho = buffer.data() + (Ndim + 1) * n_points;
+      view_host->delta = buffer.data() + (Ndim + 2) * n_points;
+      view_host->nearest_higher =
+          reinterpret_cast<int*>(buffer.data() + (Ndim + 3) * n_points);
+      view_host->cluster_index =
+          reinterpret_cast<int*>(buffer.data() + (Ndim + 4) * n_points);
+      view_host->is_seed = reinterpret_cast<int*>(buffer.data() + (Ndim + 5) * n_points);
+	  view_host->n = n_points;
 
-      // Copy memory inside the host view to device
       alpaka::memcpy(stream, view_dev, view_host);
     }
-    // Copy constructor/assignment operator
+
     PointsAlpaka(const PointsAlpaka&) = delete;
     PointsAlpaka& operator=(const PointsAlpaka&) = delete;
-    // Move constructor/assignment operator
     PointsAlpaka(PointsAlpaka&&) = default;
     PointsAlpaka& operator=(PointsAlpaka&&) = default;
-    // Destructor
     ~PointsAlpaka() = default;
 
-    cms::alpakatools::device_buffer<Device, VecArray<float, Ndim>[]> coords;
-    cms::alpakatools::device_buffer<Device, float[]> weight;
-    cms::alpakatools::device_buffer<Device, float[]> rho;
-    cms::alpakatools::device_buffer<Device, float[]> delta;
-    cms::alpakatools::device_buffer<Device, int[]> nearest_higher;
-    cms::alpakatools::device_buffer<Device, int[]> cluster_index;
-    cms::alpakatools::device_buffer<Device, int[]> is_seed;
+    cms::alpakatools::device_buffer<Device, float[]> buffer;
 
     class PointsAlpakaView {
     public:
-      VecArray<float, Ndim>* coords;
+      float* coords;
       float* weight;
       float* rho;
       float* delta;
       int* nearest_higher;
       int* cluster_index;
       int* is_seed;
+	  int n;
     };
 
     PointsAlpakaView* view() { return view_dev.data(); }
