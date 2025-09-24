fa4c91c99df2a755f606dd821f13cc6dc38d2601
Simone Balducci <simone.balducci00@gmail.com>
2025-09-24 20:47:34 +0200

    Separate functions computing clustering results from validation

--- include/CLUEstering/utils/validation.hpp
+++ include/CLUEstering/utils/validation.hpp
@@ -8,36 +8,6 @@
 
 namespace clue {
 
-  inline int compute_nclusters(std::span<const int> cluster_ids) {
-    return std::reduce(cluster_ids.begin(),
-                       cluster_ids.end(),
-                       std::numeric_limits<int>::lowest(),
-                       [](int a, int b) { return std::max(a, b); }) +
-           1;
-  }
-
-  inline std::vector<std::vector<int>> compute_clusters_points(std::span<const int> cluster_ids) {
-    const auto nclusters = compute_nclusters(cluster_ids);
-    std::vector<std::vector<int>> clusters_points(nclusters);
-
-    std::for_each(cluster_ids.begin(), cluster_ids.end(), [&, i = 0](auto cluster_id) mutable {
-      if (cluster_id > -1)
-        clusters_points[cluster_id].push_back(i++);
-    });
-
-    return clusters_points;
-  }
-
-  inline std::vector<int> compute_clusters_size(std::span<const int> cluster_ids) {
-    const auto nclusters = compute_nclusters(cluster_ids);
-    const auto clusters_points = compute_clusters_points(cluster_ids);
-
-    std::vector<int> clusters(nclusters);
-    std::ranges::transform(
-        clusters_points, clusters.begin(), [&](const auto& cluster) { return cluster.size(); });
-    return clusters;
-  }
-
   inline bool validate_results(std::span<const int> cluster_ids, std::span<const int> truth) {
     auto result_clusters_sizes = compute_clusters_size(cluster_ids);
     auto truth_clusters_sizes = compute_clusters_size(truth);
@@ -50,4 +20,4 @@ namespace clue {
     return compare_nclusters && compare_clusters_size;
   }
 
-}  // namespace clue
+}  // namespace clue::internal
