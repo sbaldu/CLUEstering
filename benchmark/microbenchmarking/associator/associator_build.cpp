5f351870afa936aac6948f29b9886fee5ede0a81
Simone Balducci <simone.balducci00@gmail.com>
2025-10-13 21:48:56 +0200

    Write first benchmark for binary associator

--- benchmark/microbenchmarking/associator/associator_build.cpp
+++ benchmark/microbenchmarking/associator/associator_build.cpp
@@ -1,9 +1,11 @@
 
 #include "CLUEstering/data_structures/internal/MakeAssociator.hpp"
+#include "CLUEstering/internal/alpaka/memory.hpp"
+#include "CLUEstering/CLUEstering.hpp"
 #include <benchmark/benchmark.h>
 #include <ranges>
 
-static void BM_AssociatorBuild(benchmark::State& state) {
+static void BM_BuildBinaryAssociatorCPU(benchmark::State& state) {
   for (auto _ : state) {
     const auto elements = state.range(0);
 	std::vector<int> associations(elements);
@@ -14,6 +16,22 @@ static void BM_AssociatorBuild(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_AssociatorBuild)->RangeMultiplier(2)->Range(1 << 10, 1 << 20);
+static void BM_BuildBinaryAssociatorAlpaka(benchmark::State& state) {
+  for (auto _ : state) {
+	auto queue = clue::get_queue(0u);
+    const auto elements = state.range(0);
+	auto h_associations = clue::make_host_buffer<int[]>(queue, elements);
+    std::ranges::transform(std::views::iota(0, elements), h_associations.data(), [](auto x) -> int32_t {
+      return x % 2 == 0;
+    });
+	auto d_associations = clue::make_device_buffer<int[]>(queue, elements);
+	alpaka::memcpy(queue, d_associations, h_associations);
+    volatile auto associator = clue::internal::make_associator(queue, std::span{d_associations.data(), elements}, elements);
+  }
+}
+
+
+BENCHMARK(BM_BuildBinaryAssociatorCPU)->RangeMultiplier(2)->Range(1 << 10, 1 << 20);
+BENCHMARK(BM_BuildBinaryAssociatorAlpaka)->RangeMultiplier(2)->Range(1 << 10, 1 << 20);
 
 BENCHMARK_MAIN();
