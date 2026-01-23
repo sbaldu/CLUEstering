5a713784d86702a632bd8797d85a5eb44af72ed8
Simone Balducci <simone.balducci00@gmail.com>
2026-01-23 18:28:47 +0100

    wip

--- include/CLUEstering/data_structures/internal/PointsCommon.hpp
+++ include/CLUEstering/data_structures/internal/PointsCommon.hpp
@@ -33,7 +33,10 @@ namespace clue {
           throw std::out_of_range("Dimension out of range in call to coords.");
         }
         auto& view = static_cast<TPoints*>(this)->m_view;
-        return std::span<float>(view.coords[dim], view.n);
+        if (!view.has_value()) {
+          throw std::logic_error("The input data passed to the points is read-only.");
+        }
+        return std::span<float>(view->coords[dim], view->n);
       }
 
       ALPAKA_FN_HOST auto weights() const {
@@ -49,39 +52,42 @@ namespace clue {
         assert(static_cast<const TPoints&>(*this).m_clustered &&
                "The points have not been clustered yet, so the cluster indexes cannot be accessed");
         auto& const_view = static_cast<const TPoints*>(this)->m_const_view;
-        return std::span<const int>(view.cluster_index, view.n);
+        return std::span<const int>(const_view.cluster_index, const_view.n);
       }
 
       ALPAKA_FN_HOST auto clustered() const {
         return static_cast<const TPoints&>(*this).m_clustered;
       }
 
-      ALPAKA_FN_HOST const auto& view() const { return static_cast<const TPoints*>(this)->m_view; }
-      ALPAKA_FN_HOST auto& view() { return static_cast<TPoints*>(this)->m_view; }
+      ALPAKA_FN_HOST const auto& view() const {
+        return static_cast<const TPoints*>(this)->m_const_view;
+      }
+      ALPAKA_FN_HOST auto& view() { return static_cast<TPoints*>(this)->m_view.value(); }
     };
 
   }  // namespace internal
 
-  template <std::size_t Ndim>
-  struct PointsView {
-    std::array<float*, Ndim> coords;
-    float* weight;
-    std::int32_t* cluster_index;
-    std::int32_t* is_seed;
-    float* rho;
-    std::int32_t* nearest_higher;
-    std::int32_t n;
+  namespace detail {
 
-    ALPAKA_FN_HOST_ACC auto operator[](int index) const {
-      if (index == -1)
-        return clue::nostd::make_array<float, Ndim + 1>(std::numeric_limits<float>::max());
+    template <std::size_t Ndim>
+    struct InputView {
+      std::array<const float*, Ndim> coords;
+      const float* weight;
+    };
+    template <std::size_t Ndim>
+    struct ConstInputView {
+      std::array<float*, Ndim> coords;
+      float* weight;
+    };
+    struct OutputView {
+      int* cluster_index;
+      int* is_seed;
+      float* rho;
+      int* nearest_higher;
+      int32_t n;
+    };
 
-      std::array<float, Ndim + 1> point;
-      meta::apply<Ndim>([&]<std::size_t Dim>() -> void { point[Dim] = coords[Dim][index]; });
-      point[Ndim] = weight[index];
-      return point;
-    }
-  };
+  }  // namespace detail
 
   template <std::size_t Ndim>
   struct ConstPointsView {
@@ -100,6 +106,27 @@ namespace clue {
       std::array<float, Ndim + 1> point;
       meta::apply<Ndim>([&]<std::size_t Dim>() { point[Dim] = coords[Dim][i]; });
       point[Ndim] = weight[i];
+      return point;
+    }
+  };
+
+  template <std::size_t Ndim>
+  struct PointsView {
+    std::array<float*, Ndim> coords;
+    float* weight;
+    std::int32_t* cluster_index;
+    std::int32_t* is_seed;
+    float* rho;
+    std::int32_t* nearest_higher;
+    std::int32_t n;
+
+    ALPAKA_FN_HOST_ACC auto operator[](int index) const {
+      if (index == -1)
+        return clue::nostd::make_array<float, Ndim + 1>(std::numeric_limits<float>::max());
+
+      std::array<float, Ndim + 1> point;
+      meta::apply<Ndim>([&]<std::size_t Dim>() -> void { point[Dim] = coords[Dim][index]; });
+      point[Ndim] = weight[index];
       return point;
     }
   };
